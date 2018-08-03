9a731f7e9b1cc620296cf541f65f3fd4c60d6482
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-08-03 12:47:25 +0100

    Use env var for connection string

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,8 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+DEFAULT_CONNECTION_STRING="Server=127.0.0.1;Port=5432;Database=FsTweet;User Id=postgres;Password=test;"
+CONNECTION_STRING=${FSTWEET_DB_CONN_STRING:-${DEFAULT_CONNECTION_STRING}}
+
 dotnet build \
     "${DIR}/src/FsTweet.Db.Migrations/FsTweet.Db.Migrations.fsproj" \
     --output "${DIR}/dist"
@@ -7,4 +10,4 @@ dotnet build \
 mono "packages/database/FluentMigrator.Console/net461/any/Migrate.exe" \
     --assembly "${DIR}/dist/FsTweet.Db.Migrations.dll" \
     --provider "Postgres" \
-    --connectionString "Server=127.0.0.1;Port=5432;Database=fstweet;User Id=postgres;Password=test;"
+    --connectionString "${CONNECTION_STRING}"
