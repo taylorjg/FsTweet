0764781a914f07713efabfbc7ec8548f811da32c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-08-30 19:05:43 +0100

    Support DATABASE_URL format in migration script

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,21 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-DEFAULT_CONNECTION_STRING="Server=localhost;Port=5432;User Id=postgres;Password=test;Database=FsTweet;"
-CONNECTION_STRING=${CONNECTION_STRING:-${DEFAULT_CONNECTION_STRING}}
+DEFAULT_DATABASE_URL="postgres://postgres:test@localhost:5432/FsTweet"
+DATABASE_URL=${DATABASE_URL:-${DEFAULT_DATABASE_URL}}
+
+USERNAME=`echo $DATABASE_URL | cut -f 3 -d '/' | cut -f 1 -d ':'`
+PASSWORD=`echo $DATABASE_URL | cut -f 3 -d '/' | cut -f 2 -d ':' | cut -f 1 -d '@'`
+HOST=`echo $DATABASE_URL | cut -f 3 -d '/' | cut -f 2 -d ':' | cut -f 2 -d '@'`
+PORT=`echo $DATABASE_URL | cut -f 3 -d '/' | cut -f 3 -d ':'`
+DATABASE=`echo $DATABASE_URL | cut -f 4 -d '/'`
+
+if [ "$1" == "-ssl" ]; then
+  CONNECTION_STRING_SUFFIX="SSL Mode=Require;Trust Server Certificate=true;"
+else
+  CONNECTION_STRING_SUFFIX=""
+fi
+
+CONNECTION_STRING="Server=$HOST;Port=$PORT;User Id=$USERNAME;Password=$PASSWORD;Database=$DATABASE;$CONNECTION_STRING_SUFFIX"
 
 dotnet build "${DIR}/src/FsTweet.Db.Migrations/FsTweet.Db.Migrations.fsproj"
 
