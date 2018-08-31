9244a41b3a96809c78cd50f5b977cdaf774ba2dd
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-08-31 01:11:07 +0100

    Not likely to work

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget https://cli-assets.heroku.com/heroku-linux-x64.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
-sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo tar -xvzf heroku-linux-x64.tar.gz -C /usr/local/lib
 sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
