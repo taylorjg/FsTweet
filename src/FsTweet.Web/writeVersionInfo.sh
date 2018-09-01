987a6f1a1c847fa6ec0cf19116da5dcc670aa5c7
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-09-01 13:50:09 +0100

    LANGUAGE

--- src/FsTweet.Web/writeVersionInfo.sh
+++ src/FsTweet.Web/writeVersionInfo.sh
@@ -1,2 +1,2 @@
-VERSION=`LANG=en_US.UTF-8 perl -n -e '/<Version>([\d.]+)<\/Version>/ && print $1' FsTweet.Web.fsproj`
+VERSION=`LANGUAGE=en_US.UTF-8 perl -n -e '/<Version>([\d.]+)<\/Version>/ && print $1' FsTweet.Web.fsproj`
 sed -i -e s/__VERSION__/$VERSION/ "$1"
